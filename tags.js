cc33ce99849e11593c6af7aa8d5149a9ab3e2eaf
Iain Dawson <iain@bldm.us>
2012-11-15 02:02:30 +0000

    Explicit unselection of hover state.

--- tags.js
+++ tags.js
@@ -9,22 +9,27 @@ $(document).ready(function() {
 
   function highlight(hash, the_class) {
     // clear selection...
-    matcher = get_matcher(false);
-
-    $(matcher).each(function(i) {
-      $(this).removeClass(the_class);
-      $(this).parents('div', 'li').slice(0, 1).removeClass(the_class);
-    });
-
-    // ...and remake it    
-    if (hash) {
-      matcher = get_matcher(hash);
-
+    if (hash == 'selection') {
+      matcher = get_matcher(false);
+      
       $(matcher).each(function(i) {
-        $(this).addClass(the_class);
-        $(this).parentsUntil('div', 'li').slice(0, 1).addClass(the_class);
+        $(this).removeClass(the_class);
+        $(this).parents('div', 'li').slice(0, 1).removeClass(the_class);
       });
     }
+
+    matcher = get_matcher(hash);
+
+    // explicitly set or unset
+    $(matcher).each(function(i) {
+      if (hash) {
+        $(this).addClass(the_class);
+        $(this).parentsUntil('div', 'li').slice(0, 1).addClass(the_class);
+      } else {
+        $(this).removeClass(the_class);
+        $(this).parentsUntil('div', 'li').slice(0, 1).removeClass(the_class);
+      }
+    });
   }
 
   hash = window.location.hash.slice(1);
